aae1848570bae8ff91b37d20b324ec4fbb8d224a
Akshita <akshita.aggarwal91@gmail.com>
2015-02-13 13:46:50 +0530

    1st commit to 1

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Akshita Aggarwal on 13/02/15.
+//  Copyright (c) 2015 Akshita Aggarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Akshita Aggarwal on 13/02/15.
+//  Copyright (c) 2015 Akshita Aggarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
